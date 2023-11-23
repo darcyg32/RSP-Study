351f4d1afc882c6a9c2328fec0ea65d47fef9627
DarcyGeyer <95901667+darcyg32@users.noreply.github.com>
2023-11-23 19:29:11 +1030

    Fully implemented binary tree

--- Implementations/BinaryTree.cpp
+++ Implementations/BinaryTree.cpp
@@ -3,16 +3,24 @@
 using namespace std;
 
 int main() {
-    BinaryTree<int> tree;
+    BinaryTree tree;
 
     // Insert some values into the tree
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(7);
-    tree.insert(2);
-    tree.insert(4);
-    tree.insert(6);
     tree.insert(8);
+    tree.insert(1);
+    tree.insert(84);
+    tree.insert(115);
+    tree.insert(45);
+    tree.insert(3);
+    tree.insert(12);
+    tree.insert(88);
+    tree.insert(11);
+    tree.insert(17);
+    tree.insert(19);
+
+    // Delete some values
+    tree.remove(12);
+    tree.remove(19);
 
     cout << "Find Min: " << tree.findMin() << "\n";
     
@@ -21,13 +29,16 @@ int main() {
     // Perform in-order traversal
     cout << "In-order traversal: ";
     tree.inOrder();
+    cout << "\n";
 
     // Perform pre-order traversal
     cout << "Pre-order traversal: ";
     tree.preOrder();
+    cout << "\n";
 
     // Perform post-order traversal
     cout << "Post-order traversal: ";
     tree.postOrder();
+    cout << "\n";
 
 }
--- Implementations/BinaryTree.hpp
+++ Implementations/BinaryTree.hpp
@@ -2,139 +2,253 @@
 #define BINARYTREE_H
 #include <iostream>
 
+// Tree node
+struct Node {
+    int value; // Data
+    int height; // Height of current node
+    struct Node *leftChild; // Pointer to node's left child
+    struct Node *rightChild; // Pointer to node's right child
 
-// Can add searching, deletion/balancing
-template <typename T>
+    // Constructor
+    Node(int x) : value(x), height(1), leftChild(nullptr), rightChild(nullptr) {}
+};
+
 class BinaryTree {
     private:
-        class Node {
-            public:
-                T item;
-                Node* parent;
-                Node* left;
-                Node* right;
-
-                // Constructor
-                Node(T x) : item(x), parent(nullptr), left(nullptr), right(nullptr) {}
-        };
-
-        Node* root;
-
-        // Helper function for insertion
-        Node* insert(Node* currNode, T x) {
-            if (currNode == nullptr) {
-                return new Node(x);
+        // Helper Getter functions
+        int getHeight(Node *N) { // Get height of tree
+            if (N == nullptr) {
+                return 0;
             }
-
-            if (x < currNode->item) {
-                currNode->left = insert(currNode->left, x);
-            } else if (x > currNode->item) {
-                currNode->right = insert(currNode->right, x);
+            return N->height;
+        }
+        
+        int getBalance(Node *N) { // Get balance-factor of node N
+            if (N == nullptr) {
+                return 0;
             }
-
-            return currNode;
+            return getHeight(N->leftChild) - getHeight(N->rightChild);
         }
 
-        // Helper function for In-order Traversal
-        void inOrder(const Node* currNode) const {
-            if (currNode != nullptr) {
-                inOrder(currNode->left);
-                std::cout << currNode->item << " ";
-                inOrder(currNode->right);
+        // Helper update height function
+        void updateHeight(Node *N) {
+            if (N == nullptr) {
+                return;
+            }
+            N->height = std::max(getHeight(N->leftChild), getHeight(N->rightChild)) + 1;
+        }
+
+        // Remove function
+        Node* remove(Node* root, int value) {
+            // Base case: If tree/subtree is empty, return nullptr
+            if (root == nullptr) {
+                return root;
+            }
+
+            // Search for node to delete
+            if (value < root->value) {
+                root->leftChild = remove(root->leftChild, value);
+            } else if (value > root->value) {
+                root->rightChild = remove(root->rightChild, value);
+            } else { // When the node with given value is found
+                // Case 1: Node with one or no children. Swap node with child node and delete original node. No need for rebalancing
+                if (root->leftChild == nullptr) {
+                    Node* temp = root->rightChild;
+                    delete root;
+                    return temp;
+                } else if (root->rightChild == nullptr) {
+                    Node* temp = root->leftChild;
+                    delete root;
+                    return temp;
+                }
+
+                // Case 2: Node with two children. May need rebalancing
+                Node* temp = maxValueNode(root->leftChild); // Find in-order successor
+                root->value = temp->value; // Copy the successor's data to this node
+                root->leftChild = remove(root->leftChild, temp->value); // Delete the successor
+            }
+
+            // Update height
+            updateHeight(root);
+
+            // Return balanced tree
+            root = balance(root);
+            return root;
+        }
+
+        // Balance functions
+        Node *rightRotate(Node *y) { // Right-rotate subtree with root y
+            Node *x = y->leftChild;
+            Node *T2 = x->rightChild;
+
+            // Perform rotation
+            x->rightChild = y;
+            y->leftChild = T2;
+
+            // Update heights
+            updateHeight(y);
+            updateHeight(x);
+
+            // Return new root
+            return x;
+        }
+
+        Node *leftRotate(Node *x) { // Left-rotate subtree with root x
+            Node *y = x->rightChild;
+            Node *T2 = y->leftChild;
+
+            // Perform rotation
+            y->leftChild = x;
+            x->rightChild = T2;
+
+            // Update heights
+            updateHeight(x);
+            updateHeight(y);
+
+            // Return new root
+            return y;
+        }
+
+        // Helper function to find the max value node from a given node (used during rebalancing)
+        Node* maxValueNode(Node* node) {
+            Node* current = node;
+            while (current->rightChild != nullptr) {
+                current = current->rightChild;
+            }
+            return current;
+        }
+
+        // Helper function to find the min value node from a given node
+        Node* minValueNode(Node* node) {
+            Node* current = node;
+            while (current->leftChild != nullptr) {
+                current = current->leftChild;
+            }
+            return current;
+        }
+
+        // Helper balancing function
+        Node* balance(Node* node) { // Perform AVL balancing after insertion/deletion
+            int balance = getBalance(node);
+
+            // If node not balanced:
+
+            // Left Left Case
+            if (balance > 1 && getBalance(node->leftChild) >= 0) {
+                return rightRotate(node);
+            }
+
+            // Left Right Case
+            if (balance > 1 && getBalance(node->leftChild) < 0) {
+                node->leftChild = leftRotate(node->leftChild);
+                return rightRotate(node);
+            }
+
+            // Right Right Case
+            if (balance < -1 && getBalance(node->rightChild) <= 0) {
+                return leftRotate(node);
+            }
+
+            // Right Left Case
+            if (balance < -1 && getBalance(node->rightChild) > 0) {
+                node->rightChild = rightRotate(node->rightChild);
+                return leftRotate(node);
+            }
+
+            // Else
+            return node;
+        }
+
+        // Helper insertion function
+        Node* insert(Node* node, int value) { // Insert value into subtree with root N, returns new root of subtree
+            // 1. Perform normal BST insertion
+            if (node == nullptr) { // Base case: If current node is empty, insert new node here
+                return new Node(value);
+            }
+            if (value < node->value) { // If current node isn't empty, and the value of the node to be inserted is less than the value of the current node, traverse to left subtree
+                node->leftChild = insert(node->leftChild, value);
+            }
+            else if (value > node->value) { // If current node isn't empty, and the value of the node to be inserted is greater than the value of the current node, traverse to right subtree
+                node->rightChild = insert(node->rightChild, value);
+            }
+            else { // Else, if value of node to be inserted is already in tree, don't insert anything, return root of original tree.
+                return node;
+            }
+
+            // 2. Update height of the inserted node
+            updateHeight(node);
+
+            // 3. Return balanced tree
+            node = balance(node);
+            return node;
+            
+        }
+
+        // Printer helper functions
+        void inOrder(Node *root) { // Print in-order traversal of tree
+            if (root != nullptr) {
+                inOrder(root->leftChild);
+                std::cout << root->value << " ";
+                inOrder(root->rightChild);
             }
         }
 
-        // Helper function for Pre-order Traversal
-        void preOrder(const Node* currNode) const {
-            if (currNode != nullptr) {
-                std::cout << currNode->item << " ";
-                preOrder(currNode->left);
-                preOrder(currNode->right);
+        void preOrder(Node *root) { // Print pre-order traversal of tree
+            if (root != nullptr) {
+                std::cout << root->value << " ";
+                preOrder(root->leftChild);
+                preOrder(root->rightChild);
             }
         }
 
-        // Helper function for Post-order Traversal
-        void postOrder(const Node* currNode) const {
-            if (currNode != nullptr) {
-                postOrder(currNode->left);
-                postOrder(currNode->right);
-                std::cout << currNode->item << " ";
-            }
-        }
-
-        // Helper function to delete all nodes
-        void destroyTree(Node* currNode) {
-            if (currNode != nullptr) {
-                destroyTree(currNode->left);
-                destroyTree(currNode->right);
-                delete currNode;
+        void postOrder(Node *root) { // Print post-order traversal of tree
+            if (root != nullptr) {
+                postOrder(root->leftChild);
+                postOrder(root->rightChild);
+                std::cout << root->value << " ";
             }
         }
 
     public:
-        // Contructor
-        BinaryTree() : root(nullptr) {}
+        Node* root; // Pointer to root node of the AVL tree
 
-        // Destructor
-        ~BinaryTree() {
-            destroyTree(root);
+        // Constructor
+        BinaryTree() {
+            root = nullptr;
         }
 
-        // Function to insert a value
-        void insert(T x) {
-            root = insert(root, x);
+        // Public insert function
+        void insert(int value) {
+            root = insert(root, value);
         }
 
-        // Function to find the min value
-        T findMin() {
-            Node* min = root;
-
-            if (root == nullptr) {
-                std::cout << "Error: Tree is Empty.\n";
-                return -1;
-            }
-
-            while (min->left != nullptr) {
-                min = min->left;
-            }
-
-            return min->item;
+        // Public remove function
+        void remove(int value) {
+            root = remove(root, value);
         }
 
-        // Function to find the max value
-        T findMax() {
-            Node* max = root;
-
-            if (root == nullptr) {
-                std::cout << "Error: Tree is Empty.\n";
-                return -1;
-            }
-
-            while (max->right != nullptr) {
-                max = max->right;
-            }
-
-            return max->item;
+        // Public find max function
+        int findMax() {
+            return maxValueNode(root)->value;
         }
 
-        // In-order Traversal
-        void inOrder() const {
+        // Public find min function
+        int findMin() {
+            return minValueNode(root)->value;
+        }
+
+        // Printers
+        void inOrder() { // Print in-order traversal of tree
             inOrder(root);
-            std::cout << "\n";
         }
 
-        // Pre-order Traversal
-        void preOrder() const {
+        void preOrder() { // Print pre-order traversal of tree
             preOrder(root);
-            std::cout << "\n";
         }
 
-        // Post-order Traversal
-        void postOrder() const {
+        void postOrder() { // Print post-order traversal of tree
             postOrder(root);
-            std::cout << "\n";
         }
-
 };
 
 #endif
